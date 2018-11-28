b44a050ecba18634c15669336dd88cc6602dc5c7
An <syan224@gmail.com>
2018-11-28 20:58:40 +0700

    add eStoreManager.sql

---
+++ eStoreManager-SERVER/eStoreManager.sql
@@ -0,0 +1,367 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: eStoreManager
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `mobile_no` varchar(255) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKrfbvkrffamfql7cjmen8v976v` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'2018-11-27 17:40:08','2018-11-27 17:40:08','ha noi','customer_1@gmail.com','0916167887','customer_1'),(2,'2018-11-27 17:44:43','2018-11-27 17:44:43','ha noi','customer_2@gmail.com','0916167887','customer_2'),(3,'2018-11-27 17:50:59','2018-11-27 17:50:59','ha noi','customer_3@gmail.com','0916167887','customer_3'),(4,'2018-11-27 18:33:45','2018-11-27 18:33:45','ha noi',NULL,'0916167887','customer_4');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `import_bill_products`
+--
+
+DROP TABLE IF EXISTS `import_bill_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_bill_products` (
+  `import_bill_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`import_bill_id`,`product_id`),
+  KEY `FKs5rl5ysvypon5n445uus5seo6` (`product_id`),
+  CONSTRAINT `FKr2afgn2oh53531yvi4jfjoshk` FOREIGN KEY (`import_bill_id`) REFERENCES `import_bills` (`id`),
+  CONSTRAINT `FKs5rl5ysvypon5n445uus5seo6` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `import_bill_products`
+--
+
+LOCK TABLES `import_bill_products` WRITE;
+/*!40000 ALTER TABLE `import_bill_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `import_bill_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `import_bills`
+--
+
+DROP TABLE IF EXISTS `import_bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_bills` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKhfxy2rvpnti184ewl45trona9` (`user_id`),
+  CONSTRAINT `FKhfxy2rvpnti184ewl45trona9` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `import_bills`
+--
+
+LOCK TABLES `import_bills` WRITE;
+/*!40000 ALTER TABLE `import_bills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `import_bills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoices` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `amount` float DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `purpose` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_bill_products`
+--
+
+DROP TABLE IF EXISTS `order_bill_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_bill_products` (
+  `order_bill_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`order_bill_id`,`product_id`),
+  KEY `FKhbju07gbk85tclhwyousa7xup` (`product_id`),
+  CONSTRAINT `FKbv4ouu9xum56sf3aeaxwxr8fw` FOREIGN KEY (`order_bill_id`) REFERENCES `order_bills` (`id`),
+  CONSTRAINT `FKhbju07gbk85tclhwyousa7xup` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_bill_products`
+--
+
+LOCK TABLES `order_bill_products` WRITE;
+/*!40000 ALTER TABLE `order_bill_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_bill_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_bills`
+--
+
+DROP TABLE IF EXISTS `order_bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_bills` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `customer_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7qcbnn0dycsqi2imnvt85bt9r` (`customer_id`),
+  KEY `FKbho64l5rr6s635nwpxj0mcs4s` (`user_id`),
+  CONSTRAINT `FK7qcbnn0dycsqi2imnvt85bt9r` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `FKbho64l5rr6s635nwpxj0mcs4s` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_bills`
+--
+
+LOCK TABLES `order_bills` WRITE;
+/*!40000 ALTER TABLE `order_bills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_bills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_types`
+--
+
+DROP TABLE IF EXISTS `product_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_types` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `barcode` varchar(40) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `price` float NOT NULL,
+  `unit` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_types`
+--
+
+LOCK TABLES `product_types` WRITE;
+/*!40000 ALTER TABLE `product_types` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `quantities` int(11) NOT NULL,
+  `product_type_id` bigint(20) NOT NULL,
+  `supplier_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrv6og3b2qlahvka0bxn7btyqd` (`product_type_id`),
+  KEY `FK6i174ixi9087gcvvut45em7fd` (`supplier_id`),
+  CONSTRAINT `FK6i174ixi9087gcvvut45em7fd` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FKrv6og3b2qlahvka0bxn7btyqd` FOREIGN KEY (`product_type_id`) REFERENCES `product_types` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_nb4h0p6txrmfc0xbrd1kglp9t` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(3,'ROLE_CASHIER'),(2,'ROLE_MANAGER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suppliers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `mobile_no` varchar(255) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKq5uvp89ra4ksaty5ghyaw4kjr` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+LOCK TABLES `suppliers` WRITE;
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FKh8ciramu9cc9q3qcqiv4ue8a6` (`role_id`),
+  CONSTRAINT `FKh8ciramu9cc9q3qcqiv4ue8a6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (4,1),(6,2),(5,3);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `mobile_no` varchar(255) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `salary` bigint(20) DEFAULT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'2018-11-27 19:10:21','2018-11-27 19:10:21',NULL,'admin@gmail.com',NULL,NULL,'$2a$10$0olCjtjthSq5O5hJXXRaoei73zsZWZGvdnRbEP0ls.GHu0nGQVVXq',NULL,'admin'),(5,'2018-11-27 19:12:06','2018-11-27 19:12:06','hoa binh','cashier_1@gmail.com','0916167887','cashier_1','$2a$10$JLSrHu2Zj0.9IGSDog8M5eMiUYc0GKzUf68o0aX.4/RgiypYcGLse',1000,'cashier 1'),(6,'2018-11-27 19:21:58','2018-11-27 19:21:58','hoa binh','manager_1@gmail.com','0916167558','manager 1','$2a$10$krIA/n7XsEirEaJZEhmhSeVKvrguABmneQ4VXcvRvh8bT7OYFmSwe',1000,'manager_1'),(9,'2018-11-28 13:47:55','2018-11-28 13:47:55','hoa binh','noone@gmail.com','0916167558','noone','$2a$10$mJC00PhyRFUEyPhQOXqZK.7nMwQLPNg8GD8paia9HIklfnBrI5o1.',0,'noone');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-28 20:58:11
