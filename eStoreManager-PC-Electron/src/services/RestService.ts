bde3c6047b7f378e39b77370611a56266b8df92f
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-12-09 16:54:47 +0700

    turn off debug mode

--- eStoreManager-PC-Electron/src/services/RestService.ts
+++ eStoreManager-PC-Electron/src/services/RestService.ts
@@ -43,12 +43,12 @@ export class RestService {
             timeout: 1000
         };
 
-        console.log("REQUESTDATA:::::::::::::");
-        console.log(postData);
+        // console.log("REQUESTDATA:::::::::::::");
+        // console.log(postData);
 
         requestElectron(options, (err: any, data: any) => {
-            console.log("DATA:::::::::::::");
-            console.log(data.body);
+            // console.log("DATA:::::::::::::");
+            // console.log(data.body);
             // console.log(err);
             if (err) {
                 cbFail({
