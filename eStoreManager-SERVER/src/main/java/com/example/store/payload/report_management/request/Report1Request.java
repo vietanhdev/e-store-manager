5ee99d7d274a5774953a67233212031d78af09b0
An <syan224@gmail.com>
2018-12-09 13:46:33 +0700

    add report manager

---
+++ eStoreManager-SERVER/src/main/java/com/example/store/payload/report_management/request/Report1Request.java
@@ -0,0 +1,23 @@
+package com.example.store.payload.report_management.request;
+
+public class Report1Request {
+
+    private String start;
+
+    private String end;
+
+    private Long length;
+
+    public String getStart() {
+        return start;
+    }
+
+    public Long getLength() {
+        return length;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+    
+}
